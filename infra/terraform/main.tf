9412fae21994f86904d32654c548d16d314ebd92
JoonKyoLee <joonkyolee@gmail.com>
2026-04-12 04:06:55 +0900

    [MCJ-86] fix: nginx dev 라우팅 및 보안 규칙 정리

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -59,22 +59,6 @@ resource "aws_security_group" "app_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    description = "Dev app"
-    from_port   = 8081
-    to_port     = 8081
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "Prod app"
-    from_port   = 8082
-    to_port     = 8082
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   egress {
     from_port   = 0
     to_port     = 0
