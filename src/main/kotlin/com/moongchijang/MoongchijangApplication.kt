38784fbfc72abb0240fc2614cb63133ae9b607ed
JoonKyoLee <joonkyolee@gmail.com>
2026-04-28 00:58:53 +0900

    [MCJ-1328] refactor: JPA Auditing 설정 및 BaseEntity 추가

--- src/main/kotlin/com/moongchijang/MoongchijangApplication.kt
+++ src/main/kotlin/com/moongchijang/MoongchijangApplication.kt
@@ -2,8 +2,10 @@ package com.moongchijang
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
+@EnableJpaAuditing
 class MoongchijangApplication
 
 fun main(args: Array<String>) {
