412006683419f840a4b7146d6c5b67c9f42b46ce
JoonKyoLee <joonkyolee@gmail.com>
2026-05-23 00:53:11 +0900

    [MCJ-1493] feat: 알림 미읽음 개수 응답 DTO 추가

---
+++ src/main/kotlin/com/moongchijang/domain/notification/application/dto/NotificationUnreadCountResponse.kt
@@ -0,0 +1,10 @@
+package com.moongchijang.domain.notification.application.dto
+
+import io.swagger.v3.oas.annotations.media.Schema
+
+@Schema(description = "미읽음 알림 개수 응답")
+data class NotificationUnreadCountResponse(
+
+    @field:Schema(description = "미읽음 알림 개수", example = "3")
+    val count: Long
+)
