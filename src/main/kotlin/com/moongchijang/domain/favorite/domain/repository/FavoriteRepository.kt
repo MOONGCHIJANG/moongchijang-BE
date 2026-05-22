5ee08a1e2257a5347f1b77440e5f6b28dfd644e2
JoonKyoLee <joonkyolee@gmail.com>
2026-05-22 15:11:31 +0900

    [MCJ-1491] feat: 찜 목록 필터/정렬 지원 repository 조회 메소드 추가

--- src/main/kotlin/com/moongchijang/domain/favorite/domain/repository/FavoriteRepository.kt
+++ src/main/kotlin/com/moongchijang/domain/favorite/domain/repository/FavoriteRepository.kt
@@ -3,7 +3,7 @@ package com.moongchijang.domain.favorite.domain.repository
 import com.moongchijang.domain.favorite.domain.entity.Favorite
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface FavoriteRepository : JpaRepository<Favorite, Long> {
+interface FavoriteRepository : JpaRepository<Favorite, Long>, FavoriteRepositoryCustom {
 
     fun existsByUserIdAndGroupBuyId(userId: Long, groupBuyId: Long): Boolean
 
---
+++ src/main/kotlin/com/moongchijang/domain/favorite/domain/repository/FavoriteRepositoryCustom.kt
@@ -0,0 +1,22 @@
+package com.moongchijang.domain.favorite.domain.repository
+
+import com.moongchijang.domain.favorite.application.dto.WishFilterType
+import com.moongchijang.domain.favorite.application.dto.WishSortType
+import com.moongchijang.domain.groupbuy.domain.entity.GroupBuy
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+
+interface FavoriteRepositoryCustom {
+
+    companion object {
+        const val ALMOST_ACHIEVED_RATE = 80
+        const val CLOSING_SOON_DAYS = 3L
+    }
+
+    fun findWishlistGroupBuys(
+        userId: Long,
+        filter: WishFilterType,
+        sort: WishSortType,
+        pageable: Pageable,
+    ): Page<GroupBuy>
+}
