ed83bb6a09a365e6b2574e149a1d6b8ebbf27e5a
JoonKyoLee <joonkyolee@gmail.com>
2026-05-25 01:48:36 +0900

    [MCJ-1517] chore: 미사용 notifications trigger_type 인덱스 제거

--- src/main/resources/db/migration/V23__alter_notifications_add_trigger_type.sql
+++ src/main/resources/db/migration/V23__alter_notifications_add_trigger_type.sql
@@ -1,5 +1,2 @@
 ALTER TABLE notifications
     ADD COLUMN trigger_type VARCHAR(50) NULL AFTER deeplink_type;
-
-CREATE INDEX idx_notifications_user_trigger_type_occurred_at
-    ON notifications (user_id, trigger_type, occurred_at DESC);
