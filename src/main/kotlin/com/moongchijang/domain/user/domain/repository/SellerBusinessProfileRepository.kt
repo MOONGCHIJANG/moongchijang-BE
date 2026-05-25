27e847657527b1b0187c86315d2166377c162cb4
JoonKyoLee <joonkyolee@gmail.com>
2026-05-25 22:58:24 +0900

    [MCJ-1523] feat: 사장님 가입 정보 저장용 리포지토리 및 스키마 추가

---
+++ src/main/kotlin/com/moongchijang/domain/user/domain/repository/SellerBusinessProfileRepository.kt
@@ -0,0 +1,10 @@
+package com.moongchijang.domain.user.domain.repository
+
+import com.moongchijang.domain.user.domain.entity.SellerBusinessProfile
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface SellerBusinessProfileRepository : JpaRepository<SellerBusinessProfile, Long> {
+    fun findByUserId(userId: Long): SellerBusinessProfile?
+
+    fun existsByBusinessRegistrationNumber(businessRegistrationNumber: String): Boolean
+}
