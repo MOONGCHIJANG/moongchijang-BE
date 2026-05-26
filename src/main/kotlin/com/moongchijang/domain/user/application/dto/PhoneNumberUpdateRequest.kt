d59c1ae80f17373e3900a2a2fc1883841f240bfa
JoonKyoLee <joonkyolee@gmail.com>
2026-05-26 14:58:23 +0900

    [MCJ-1531] fix: 전화번호 변경 포맷 검증 규칙 일치화

--- src/main/kotlin/com/moongchijang/domain/user/application/dto/PhoneNumberUpdateRequest.kt
+++ src/main/kotlin/com/moongchijang/domain/user/application/dto/PhoneNumberUpdateRequest.kt
@@ -9,7 +9,7 @@ data class PhoneNumberUpdateRequest(
 
     @field:NotBlank(message = "전화번호는 필수입니다.")
     @field:Pattern(
-        regexp = "^01[0-9]-?\\d{3,4}-?\\d{4}$",
+        regexp = "^01[0-9]-[0-9]{3,4}-[0-9]{4}$",
         message = "올바른 전화번호를 입력해주세요.",
     )
     @field:Schema(description = "변경할 전화번호", example = "010-1234-5678")
