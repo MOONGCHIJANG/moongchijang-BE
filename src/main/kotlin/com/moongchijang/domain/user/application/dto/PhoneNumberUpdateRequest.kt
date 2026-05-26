1d6302f6cda0930e4e094780003dbc89b7870793
JoonKyoLee <joonkyolee@gmail.com>
2026-05-26 14:28:14 +0900

    [MCJ-1531] feat: 계정 설정(닉네임/전화번호/비밀번호) 변경 DTO 추가

---
+++ src/main/kotlin/com/moongchijang/domain/user/application/dto/PhoneNumberUpdateRequest.kt
@@ -0,0 +1,17 @@
+package com.moongchijang.domain.user.application.dto
+
+import io.swagger.v3.oas.annotations.media.Schema
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Pattern
+
+@Schema(description = "전화번호 변경 요청")
+data class PhoneNumberUpdateRequest(
+
+    @field:NotBlank(message = "전화번호는 필수입니다.")
+    @field:Pattern(
+        regexp = "^01[0-9]-?\\d{3,4}-?\\d{4}$",
+        message = "올바른 전화번호를 입력해주세요.",
+    )
+    @field:Schema(description = "변경할 전화번호", example = "010-1234-5678")
+    val phoneNumber: String,
+)
