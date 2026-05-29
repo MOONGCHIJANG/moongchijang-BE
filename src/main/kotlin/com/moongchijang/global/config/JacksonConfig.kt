7f706bb4107bf427291952143c95565ce7a4ab1a
JoonKyoLee <joonkyolee@gmail.com>
2026-05-29 12:30:55 +0900

    [MCJ-1559] fix: ObjectMapper 빈을 Jackson2ObjectMapperBuilder 기반으로 변경

--- src/main/kotlin/com/moongchijang/global/config/JacksonConfig.kt
+++ src/main/kotlin/com/moongchijang/global/config/JacksonConfig.kt
@@ -3,9 +3,10 @@ package com.moongchijang.global.config
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
 
 @Configuration
 class JacksonConfig {
     @Bean
-    fun objectMapper(): ObjectMapper = ObjectMapper()
+    fun objectMapper(builder: Jackson2ObjectMapperBuilder): ObjectMapper = builder.build()
 }
